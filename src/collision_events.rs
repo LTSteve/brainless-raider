cf67f499e5f87472d1f60d73c24c3bd4db7c68d0
LTSteve <steth1000@gmail.com>
2024-08-20 05:19:02 -0400

    delta

--- src/collision_events.rs
+++ src/collision_events.rs
@@ -5,7 +5,7 @@ use bevy::{ecs::system::EntityCommands, prelude::*};
 
 // Constants
 
-pub const GOBLINOID_DEATH_OFFSET: f32 = 1.0; // TODO: this is odd
+pub const GOBLINOID_DEATH_OFFSET: f32 = 1.0;
 pub const GOBLINOID_DEATH_SCALE: f32 = 0.8;
 pub const GOBLINOID_DEATH_COLOR: Color = Color::GRAY;
 pub const GOBLINOID_DEATH_ROTATION: f32 = 90.0;
