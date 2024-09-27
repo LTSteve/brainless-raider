45d948a884bbeee4701c16e1c46b708ca216b0b4
LTSteve <steth1000@gmail.com>
2024-09-27 08:21:27 -0400

    add background loop for real

--- src/ui.rs
+++ src/ui.rs
@@ -1,14 +1,18 @@
 use bevy::{ecs::system::EntityCommands, prelude::*};
 use bevy_utils::HashMap;
 
-use crate::{BackgroundLoop, ComponentHydrators, NoTearDown, ObjectData};
+use crate::{
+    audio_server, AudioServer, BackgroundLoop, ComponentHydrators, NoTearDown, ObjectData,
+    Uninintialized,
+};
 
 // Plugin
 
 pub struct UIPlugin;
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_hydrators);
+        app.add_systems(Startup, add_hydrators)
+            .add_systems(Update, initialize_background_loop);
     }
 }
 
@@ -25,3 +29,18 @@ pub struct TreasuresLabel;
 // Systems
 
 fn add_hydrators(mut hydrators: ResMut<ComponentHydrators>) {}
+
+fn initialize_background_loop(
+    mut commands: Commands,
+    audio_server: Option<Res<AudioServer>>,
+    background_loop_q: Query<Entity, (With<BackgroundLoop>, With<Uninintialized>)>,
+) {
+    if let Ok(entity) = background_loop_q.get_single() {
+        if audio_server.is_none() {
+            return;
+        }
+        let audio_server = audio_server.unwrap();
+        commands.spawn(audio_server.dumbraider.create_loop());
+        commands.entity(entity).remove::<Uninintialized>();
+    }
+}
